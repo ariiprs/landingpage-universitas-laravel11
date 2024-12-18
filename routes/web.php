e4acd21c59383613355ee0c57eb277e9cc6d0530
Arie Prasetyo <arieprasetyo987@gmail.com>
2024-12-18 16:47:11 +0700

    commit front end project

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('front.dashboard');
+})->name('front.dashboard');
+
+Route::get('/categories', function () {
+    return view('front.categories');
+})->name('front.categories');
+
+Route::get('/details', function () {
+    return view('front.details');
+})->name('front.details');
+
+Route::get('/search', function () {
+    return view('front.search');
+})->name('front.search');
