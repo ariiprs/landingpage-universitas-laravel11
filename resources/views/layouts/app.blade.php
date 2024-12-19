9a94daf9857d3575128a33077626fd90fcd1a841
Arie Prasetyo <arieprasetyo987@gmail.com>
2024-12-19 14:38:40 +0700

    first add filament to project

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,5 +15,7 @@
 
         <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
         <script src="{{ asset ('js/swipe.js') }}"></script>
-</body>
+
+
+    </body>
 </html>
